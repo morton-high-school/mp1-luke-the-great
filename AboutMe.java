e37a8664a12ecc06c5bbbbc8cf7f5651d200997d
luke-the-great <luke.baker@mcusd709.org>
2024-09-03 09:02:09 -0500

    Update AboutMe.java

--- AboutMe.java
+++ AboutMe.java
@@ -1,5 +1,5 @@
 public class AboutMe{
-  public static void main(string[] args){
+  public static void main(String[] args){
   System.out.println("My name is Luke");
     System.out.println("i am in the eleventh grade");
     System.out.println("i am in computer science a this year");
